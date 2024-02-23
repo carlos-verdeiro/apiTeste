bd5c76d00da2285e3dddab9513be1471f20de7d8
CarlosDanielVerdeiro <carlos.d.verdeiro@gmail.com>
2024-02-23 00:54:13 -0300

    API Implementada

--- api.php
+++ api.php
@@ -6,21 +6,21 @@ $stmt = $conn->query("SELECT * FROM pessoas");
 
 $usuarios = $stmt->fetchAll();
 
-//Verificando qual o método (procura-se GET)
+// Verificando qual o método (procura-se GET)
 $metodo = $_SERVER['REQUEST_METHOD'];
 
-//Transformando em array
+// Transformando em array
 $requisicao = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 
-//Método de resposta
+// Método de resposta
 header('Content-Type: application/json');
 
 if ($requisicao[0] == 'usuarios') {
     echo json_encode($usuarios);
-}elseif ($requisicao[0] == 'usuario') {
+} elseif ($requisicao[0] == 'usuario') {
     $id = $requisicao[1]; 
     $found = false;
-    foreach($usuarios as $usuario){
+    foreach($usuarios as $usuario) {
         if ($usuario['id'] == $id) {
             echo json_encode($usuario);
             $found = true;
@@ -30,7 +30,7 @@ if ($requisicao[0] == 'usuarios') {
     if (!$found) {
         echo json_encode(['erro' => 'Usuario não encontrado']);
     }
-}
-else{
+} else {
     echo json_encode(['erro' => 'Caminho não encontrado']);
 }
+?>
